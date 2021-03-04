9326b4fb72dbdad1087e136a8ab8d2f01ed5cd48
TihomirGalov <titi02@mail.bg>
2021-03-04 15:53:53 +0200

    cron fix

--- stores/cron.py
+++ stores/cron.py
@@ -5,7 +5,7 @@ from .scraper import kaufland, billa, lidl
 
 def get_data():
     for store in Store.objects.all():
-        if store.promotions.last().expires <= datetime.datetime.now():
+        if store.promotions.filter(expires__lte=datetime.datetime.now()).count() == store.promotions.all().count():
             if store.name == "Billa":
                 billa(store)
             if store.name == "Kaufland":
