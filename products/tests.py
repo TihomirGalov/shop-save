ac8786455830a4aa340580ec19ad438df1065262
TihomirGalov <titi02@mail.bg>
2021-03-05 21:12:25 +0200

    tests and gnu

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Promotion, Product
+
+
+class PromotionTestCase(TestCase):
+
+    def promotion_items(self):
+        for promotion in Promotion.objects.all():
+            self.assertNotEqual(promotion.items.all().count(), 0,
+                                f"The promotion with id {promotion.pk} is  without products in it")
+
+
+class ProductTestCase(TestCase):
+
+    def product_price(self):
+        self.assertNotEqual(Product.objects.filter(preice__lte=0), 0, "The price of a product  is negative")
--- stores/tests.py
+++ stores/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Store
 
-# Create your tests here.
+
+class StoresTestCase(TestCase):
+
+    def test_all_stores_are_available(self):
+        names = {'Billa', 'Lidl', 'Kaufland'}
+        for store in Store.objects.all():
+            self.assertIn(store.name, names, f"The store {store.pk} is not supported or a store is repeated")
+            names.remove(store.name)
